aa5d57928468920f5f3e96d01b01a06577a4dab2
asaadmansour <as89aadmansour@gmail.com>
2026-04-11 20:23:49 +0200

    added router and redux

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM node:22.14.0-alpine
 WORKDIR /app
 COPY package.json package-lock.json ./
+RUN npm install 
 COPY . .
 EXPOSE 5173
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.tsx'
-
 createRoot(document.getElementById('root')!).render(
+  //add redux here
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    {/* <Provider store={}> */}
+      <App />
+    {/* </Provider> */}
+    </BrowserRouter>
   </StrictMode>,
 )
