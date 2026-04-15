88015fb823e921134a51871953d6a367055320b0
asaadmansour <as89aadmansour@gmail.com>
2026-04-16 01:23:46 +0200

    added css variables

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
-import { Provider } from 'react-redux'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App.tsx';
 createRoot(document.getElementById('root')!).render(
   //add redux here
   <StrictMode>
     <BrowserRouter>
-    {/* <Provider store={}> */}
+      {/* <Provider store={}> */}
       <App />
-    {/* </Provider> */}
+      {/* </Provider> */}
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
